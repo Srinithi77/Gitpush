95c3094692ac8f78c3a8bd26405e73b7f9426ccf
Srinithi <aponnalaki@gmail.com>
2022-11-18 22:54:20 +0530

    First commit

---
+++ some.php
@@ -0,0 +1,12 @@
+<html>
+    <body>
+        <form method="post" action="some.php">
+         New password: <input type="email" name="upass">
+         Confirm password: <input type="password" name="cpass">
+         <input type="submit" name="sname">
+        </form>
+    </body>
+    <?php
+        $upass = $_POST['upass']
+    ?>
+</html>
